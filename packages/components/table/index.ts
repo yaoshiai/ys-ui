bb23f8073ab28b7966a0faf264a1a74d0563b664
yaoshiai <857990739@qq.com>
2022-10-27 15:25:01 +0800

    refactor(project): test file

---
+++ packages/components/table/index.ts
@@ -0,0 +1,26 @@
+import { withInstall, withNoopInstall } from '@element-plus/utils'
+import Table from './src/table.vue'
+import TableColumn from './src/tableColumn'
+
+export const ElTable = withInstall(Table, {
+  TableColumn,
+})
+export default ElTable
+export const ElTableColumn = withNoopInstall(TableColumn)
+
+export type TableInstance = InstanceType<typeof Table>
+
+export type TableColumnInstance = InstanceType<typeof TableColumn>
+
+export type {
+  SummaryMethod,
+  Table,
+  TableProps,
+  TableRefs,
+  ColumnCls,
+  ColumnStyle,
+  TreeNode,
+  RenderRowData,
+  Sort,
+  Filter,
+} from './src/table/defaults'
